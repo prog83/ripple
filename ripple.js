e40b0b883538d762f1170f4d652a3616a5675d8b
o.myronov.1607 <o.myronov.1607@in.np.gov.ua>
2020-02-24 19:25:20 +0200

    support old browser

--- ripple.js
+++ ripple.js
@@ -1,15 +1,16 @@
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll("button");
 
 function createRipple(e) {
-  const circle = document.createElement('div');
-  circle.classList.add('ripple');
+  const circle = document.createElement("div");
+  circle.classList.add("ripple");
   this.appendChild(circle);
 
   const d = Math.max(this.clientWidth, this.clientHeight);
-  //   circle.style.width = circle.style.height = `${d}px`;
-  circle.style.left = `${e.clientX - this.offsetLeft - d / 2}px`;
-  circle.style.top = `${e.clientY - this.offsetTop - d / 2}px`;
-  console.log(this.offsetLeft, this.offsetTop);
+  circle.style.width = circle.style.height = d + "px";
+  circle.style.left = e.clientX - this.offsetLeft - d / 2 + "px";
+  circle.style.top = e.clientY - this.offsetTop - d / 2 + "px";
 }
 
-buttons.forEach(btn => btn.addEventListener('click', createRipple));
+Array.prototype.slice.call(buttons).forEach(function(btn) {
+  btn.addEventListener("click", createRipple);
+});
