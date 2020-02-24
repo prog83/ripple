da8335269f4524bf389cb813bbeb1354d6e97e90
prog3 <prog3@mail@gmail.com>
2020-02-24 07:20:27 +0200

    init

---
+++ ripple.js
@@ -0,0 +1,15 @@
+const buttons = document.querySelectorAll('button');
+
+function createRipple(e) {
+  const circle = document.createElement('div');
+  circle.classList.add('ripple');
+  this.appendChild(circle);
+
+  const d = Math.max(this.clientWidth, this.clientHeight);
+  //   circle.style.width = circle.style.height = `${d}px`;
+  circle.style.left = `${e.clientX - this.offsetLeft - d / 2}px`;
+  circle.style.top = `${e.clientY - this.offsetTop - d / 2}px`;
+  console.log(this.offsetLeft, this.offsetTop);
+}
+
+buttons.forEach(btn => btn.addEventListener('click', createRipple));
